a6b899a94375906104ee3fcf04a3a5352c65b494
Ravish Rawal <rr2914@columbia.edu>
2017-07-13 19:32:58 -0400

    Cant GET stylesheets

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require( 'express' );
 const app = express();
 const nunjucks = require('nunjucks');
+const tweetBank = require('./tweetBank.js')
+const routes = require('./routes');
+app.use('/',routes);
 app.set('view engine','html');
 app.engine('html',nunjucks.render);
-var locals = {
-  title: "An Example",
-  people: [
-    {name: 'Gandalf'},
-    {name: 'Frodo'},
-    {name:'Hermione'}
-  ]
-};
 
+app.use('/',function(req,res,next){
+  next();
+})
 nunjucks.configure('views',{noCache:true});
-nunjucks.render('index.html',locals,function(err,output){
-  console.log(output);
-});
-
-
-
 var port = 3000 || process.env.PORT
 app.listen(port,function(){
   console.log('server listening');
@@ -31,9 +23,16 @@ app.use('/news',function(req,res,next){
   console.log('You\'ve reached news')
   next();
 })
-app.get('/',function(req,res){
-  res.send('Welcome!')
-})
-app.get('/news',function(req,res){
-  res.send('fake news');
-})
+
+var userCmd='list';
+tweetBank[userCmd]();
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.get('/stylesheets/style.css',function(req,res,next){
+  res.sendFile('/stylesheets/style.css');
+  next();
+});
+
+// nunjucks.render('index.html',locals,function(err,output){
+//   console.log(output);
+// });
